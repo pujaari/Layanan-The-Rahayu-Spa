e407965200e54eb9f1f19cd8eccdbbc13fa50ef2
Ni Kadek Puja Ari Pertiwi <95269918+pujaari@users.noreply.github.com>
2021-12-07 10:17:26 +0800

    Add files via upload

---
+++ header.h
@@ -0,0 +1,27 @@
+//=======================================================================//
+//***********      Fungsi Untuk Menampilkan Header Program   *********** //
+//=======================================================================//
+// Nama Fungsi    : header                                               //
+// Input Argumen  : void                                                 //
+// Output Argumen : void                                                 //
+// Deskripsi      : Fungsi ini untuk menampilkan header program. Berisi  //
+//                  informasi berupa nama, alamat, no.telp.              //
+// Dibuat Oleh    : Jenni - 2105551028                                   //
+//                                                                       //
+// Versi : 1.1                                     Rev. 1                //
+// Tgl   : 30-11-2020                               Tgl: 01-12-2021      //
+// Revisi 1       : Merapikan isi fungsi                                 //
+// Direvisi Oleh  : Puja - 2105551016                                    //
+//=======================================================================//
+//fungsi untuk menampilkan header program
+void header(void){
+    printf("\n\n\n");
+    printf("\t\t\t\t\t==============================================\n");
+	printf("\t\t\t\t\t|              'THE RAHAYU SPA'              |\n");
+	printf("\t\t\t\t\t|============================================|\n");
+	printf("\t\t\t\t\t|   Jalan Bintang No. 295, Kota Galaksi      |\n");
+	printf("\t\t\t\t\t|   Relaxtion your body with Rahayu Spa      |\n");
+	printf("\t\t\t\t\t|    >>> Kenyamanan Anda Yang Utama <<<      |\n");
+	printf("\t\t\t\t\t==============================================\n");
+
+}

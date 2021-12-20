4aafba61b1653b0b579044a033190437e899af07
Ni Kadek Puja Ari Pertiwi <95269918+pujaari@users.noreply.github.com>
2021-12-20 21:50:33 +0800

    Add files via upload

---
+++ judul.h
@@ -0,0 +1,49 @@
+//=======================================================================//
+//***********         Fungsi Untuk Menampilkan Judul     ****************//
+//=======================================================================//
+// Nama Fungsi    : judul                                                //
+// Input Argumen  : void                                                 //
+// Output Argumen : void                                                 //
+// Deskripsi      : Fungsi ini untuk menampilkan judul program. Berisi   //
+//                  informasi program ini dibuat oleh siapa dan halaman  //
+//                  awal bagian pemesanan dengan nama spa.               //
+// Dibuat Oleh    : Puja - 2105551016                                    //
+//                                                                       //
+// Versi : 1.1                                     Rev. 1                //
+// Tgl   : 1-12-2021                               Tgl: 22-12-2021       //
+// Revisi 1       : Memperlengkap isinya dan mempercantik. Menambah      //
+//                  system color, keterangan informasi program, dan      //
+//                  halaman awal bagian pemesanan.                       //
+// Direvisi Oleh  :  Jenni - 2105551028                                  //
+//=======================================================================//
+void judul(void){
+	system("cls");
+	system("color 20");
+	printf("\n\n\n\n\n\n\n");
+	printf("\t\t\t\t==================================================\n");
+	printf("\t\t\t\t|            Program Layanan The Rahayu SPA      | \n");
+	printf("\t\t\t\t==================================================\n");
+	printf("\t\t\t\t|            This Program Created By :           |\n");
+	printf("\t\t\t\t|                    1. Puja                     |\n");
+	printf("\t\t\t\t|                    2. Jenni                    |\n");
+	printf("\t\t\t\t==================================================\n\n\n");
+	printf("\t\t\t\t\t\t**********************\n");
+	printf("\t\t\t\t\t\t|Click enter to start|\n");
+	printf("\t\t\t\t\t\t**********************\n");
+	getch();
+	system("cls");
+	printf("\t\n\n\n\n\n\n\n\n"); //Halaman awal bagian pemesanan dengan nama hotel
+	printf("\t\t\t\t\t==============================================\n");
+	printf("\t\t\t\t\t|      SELAMAT DATANG DI RAHAYU SPA          |\n");
+	printf("\t\t\t\t\t|       'WELCOME TO THE RAHAYU SPA'          |\n");
+	printf("\t\t\t\t\t|============================================|\n");
+	printf("\t\t\t\t\t|   Jalan Bintang No. 295, Kota Galaksi      |\n");
+	printf("\t\t\t\t\t|   Relaxtion your body with Rahayu Spa      |\n");
+	printf("\t\t\t\t\t|    >>> Kenyamanan Anda Yang Utama <<<      |\n");
+	printf("\t\t\t\t\t|    Hubungi  : 08123456789                  |\n");
+	printf("\t\t\t\t\t|   Instagram : @rahayu.spa                  |\n");
+	printf("\t\t\t\t\t|   Facebook  : rahayuspabali                |\n");
+	printf("\t\t\t\t\t|   E-mail    : rahayuspa@gmail.com          |\n");
+	printf("\t\t\t\t\t==============================================\n");
+	getch();
+}

a467c5b1592c23b4413c2d36581f08042daac115
Jenni <90305383+jenniferputria@users.noreply.github.com>
2021-12-28 10:51:12 +0800

    Update tampilkanRincianJasa1.h

--- tampilkanRincianJasa1.h
+++ tampilkanRincianJasa1.h
@@ -14,14 +14,17 @@
 // Revisi 1       : Merapikan isi fungsi supaya lebih nyaman dilihat.    //
 // Direvisi Oleh  :  Jenni - 2105551028                                  //
 //=======================================================================//
+//fungsi untuk menampilkan rincian jasa
 void tampilkanRincianJasa1(void){
-	printf("|****************************************************************************************************|\n");
-	printf("|                                            ~REFLEXY~                                               |\n");
-	printf("|			                  Foot Massage With Shooting Aromatherapy		 	     	             |\n");
-	printf("|                             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^         			             |\n");
-	printf("|      		   Manjakan diri, hilangkan penat dan rasa lelah pada kaki cantik anda. 	             |\n");
-	printf("|			                cocok untuk anda yang aktif bergerak seharian				             |\n");
-	printf("|  Perawatan ini difokuskan pada kaki anda, yaitu pijat refleksi kaki dengan aromaterapi menenangkan |\n");
-	printf("|****************************************************************************************************|\n");
-	printf("|                                      | 1.5 hours Rp 190.000,- |                                    |\n");
-	printf("|****************************************************************************************************|\n");
+    printf("|****************************************************************************************************|\n");
+    printf("|                                            <REFLEXY>                                               |\n");
+    printf("|****************************************************************************************************|\n");
+    printf("|                             Foot Massage With Shooting Aromatherapy                                |\n");
+    printf("|                             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                |\n");
+    printf("|              Manjakan diri, hilangkan penat dan rasa lelah pada kaki cantik anda.                  |\n");
+    printf("|                         cocok untuk anda yang aktif bergerak seharian                              |\n");
+    printf("|  Perawatan ini difokuskan pada kaki anda, yaitu pijat refleksi kaki dengan aromaterapi menenangkan |\n");
+    printf("|****************************************************************************************************|\n");
+    printf("|                                  | 1.5 hours Rp 190.000,- |                                        |\n");
+    printf("|****************************************************************************************************|\n");
+}

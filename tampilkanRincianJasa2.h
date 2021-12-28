6c1e7724e8bc4a5f9844409c47293e4b64f82295
Jenni <90305383+jenniferputria@users.noreply.github.com>
2021-12-28 10:51:58 +0800

    Update tampilkanRincianJasa2.h

--- tampilkanRincianJasa2.h
+++ tampilkanRincianJasa2.h
@@ -15,15 +15,15 @@
 // Direvisi Oleh  :  Jenni - 2105551028                                  //
 //=======================================================================//
 void tampilkanRincianJasa2(void){
-	printf("|****************************************************************************************************|\n");
-	printf("|                                          ~REJUVENATE~                                              |\n");
-	printf("|			            Relaxing Back Massage With Gentle Touch Of Olive Oil                         |\n");
-	printf("|                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                         |\n");
-	printf("| Kembalikan rasa energik anda, dapatkan relaksasi yang menyegarkan di setiap sentuhan minyak zaitun |\n");
-	printf("| Perawatan ini akan mengurangi rasa pegal & kelelahan pada punggung dengan pijatan lembut olive oil |\n");
-	printf("|    Sangat cocok bagi anda yang duduk seharian saat bekerja. pijatan punggung yang lembut dengan    |\n");
-	printf("|                        minyak zaitun ini akan mengembalikan vitalitas anda.                        |\n");
-	printf("|****************************************************************************************************|\n");
-	printf("|                                       | 1.5 Hours Rp 205.000,- |                                   |\n");
-	printf("|****************************************************************************************************|\n");
+    printf("|****************************************************************************************************|\n");
+    printf("|                                          <REJUVENATE>                                              |\n");
+    printf("|                       Relaxing Back Massage With Gentle Touch Of Olive Oil                         |\n");
+    printf("|                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                         |\n");
+    printf("| Kembalikan rasa energik anda, dapatkan relaksasi yang menyegarkan di setiap sentuhan minyak zaitun |\n");
+    printf("| Perawatan ini akan mengurangi rasa pegal & kelelahan pada punggung dengan pijatan lembut olive oil |\n");
+    printf("|   Sangat cocok bagi anda yang duduk seharian saat bekerja. pijatan punggung yang lembut dengan     |\n");
+    printf("|                      minyak zaitun ini akan mengembalikan vitalitas anda.                          |\n");
+    printf("|****************************************************************************************************|\n");
+    printf("|                                     | 1.5 Hours Rp 205.000,- |                                     |\n");
+    printf("|****************************************************************************************************|\n");
 }
